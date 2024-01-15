c777bbd5c97fb1fb712c82ce5fb328f0891b0fc9
amirreza473 <amireza4377@gmail.com>
2024-01-16 00:16:35 +0330

    login page dev

--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -3,11 +3,14 @@
 
 function Login() {
     return (
-        <div className="bg-zinc-800 h-screen">
-            <div className="grid grid-cols-3">
-                <form className="bg-gray-500">
+        <div className="bg-zinc-800 h-screen overflow-hidden">
+            <h1 className="text-center text-5xl text-white translate-y-36">Lorem UI</h1>
+            <div className="flex justify-center">
+                <form className="bg-white flex flex-col space-y-5 my-44 px-10 py-20 rounded-md">
                     <input type="text" className="border p-2  rounded-lg shadow-sm placeholder-slate-400 focus:outline-none focus:border-rose-400 focus:ring-rose-400 focus:ring-2" name="username" placeholder="username" />
                     <input type="text" className="border p-2  rounded-lg shadow-sm placeholder-slate-400 focus:outline-none focus:border-rose-400 focus:ring-rose-400 focus:ring-2" name="username" placeholder="password" />
+                    <a className="text-sm text-blue-600 underline decoration-blue-500 hover:text-blue-800 hover:decoration-blue-800" href="#">have you forget your password?</a>                    
+                    <button className="bg-rose-400 text-white p-1 rounded-md hover:bg-zinc-800">Login</button>                    
                 </form>
             </div>
         </div>
